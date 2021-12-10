d42c312fead0065e1329979bf78d454084d02c00
JohanCarrillo <johanime1907@gmail.com>
2021-12-09 20:41:16 -0500

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TOD-ROCK-PAPER-SCISSORS
+This project was proposed at The Odin Project to put in practice the acquired 
+abilites in javascript. I found it boring to do a simple Rock-Paper-Scissors
+game so I changed it a bit with a cat's topic I found more interesting.
+
+### Learned outcomes:
+The most interesting thing I learned here is that even if a eventLinstener is 
+used inside a function, the event will remain even if the execution of the
+function ends.
+I also learned to hide and show parts of the webpage by changing the display 
+attribute.
+I practiced the basic DOM manipulation using scripts.
+
+### Credits for the images:
+Boss cat pictures: https://www.boredpanda.com/the-adventures-of-business-cat-comics-tom-fonder/
+Cat punch gif: https://c.tenor.com/O6nMWd3Zv5cAAAAC/cat-punch.gif
+Cat punch, scratch and slap draws were made by me =D
--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function computerPlay() {
   // Simulates the computer play, choosing a random election
 
   let computer_election = Math.floor(Math.random() * 2.99);
-
+  /* [0, 0.99] --> rock
+     [1, 1.99] --> paper
+     [2, 2.99] --> scissors
+  */
   if (computer_election === 0) {
     return 'Rock';
   } else if (computer_election === 1) {
@@ -12,103 +15,103 @@ function computerPlay() {
   }
 }
 
-function Standarize(word) {
-  // this function capitalize the first char and make lower all the others in a given str
-
-  const wordarray = word.split('');  // converts to array
-  // convert all letters to lower
-  for (let i = 0; i < wordarray.length; i++) {   
-    wordarray[i] = wordarray[i].toLowerCase();
-  }
-  wordarray[0] = wordarray[0].toUpperCase();  // capitalize first char
-  word = wordarray.join('');  // convert to string
-  /*
-  word = wordarray.toString();  // converts to string again
-  word = word.replace(/,/g, '');  // deletes semi-colons
-  */
-  return(word);
+function showMessage(message) {
+  // show a message after every match
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.textContent = message;
+  const container = document.querySelector('#container2');
+  container.appendChild(div);
+  // should I delete last message or let it accumalte like a history? dunno
 }
 
 function playRound(playerSelection, computerSelection) {
   /* This function simulates a game of rock-paper-scissors, comparing
-      the player election and computer election to declare which one wins.
-      The rules of the game are the standards*/
-  
-  playerSelection = Standarize(playerSelection);
-  let winner;
+      the player's election and computer's election to declare which one wins.
+  */
+  //console.log('Computer chose: ' + computerSelection
+  //    + ' You chose: ' + playerSelection);  // this was made to track who win
+  let winner, message;
 
   switch (true) {
     case playerSelection === computerSelection: 
-      console.log('Draw!');
+      message = 'Draw!';
       winner = 'no-one';
       break;
     case (playerSelection === 'Rock' && computerSelection === 'Paper'):
-      console.log('You fool! Paper beats Rock!');
+      message = 'You fool! Cat Slap beats Cat Punch!';
       winner = 'computer';
       break;
     case (playerSelection === 'Rock' && computerSelection === 'Scissors'):
-      console.log('You\'re pretty good! Scissors beat Rock!');
+      message = 'Lucky hooman! Cat Scratch beat Cat Punch!';
       winner = 'player';
       break;
     case (playerSelection === 'Paper' && computerSelection === 'Rock'):
-      console.log('You got lucky this time! Paper beats Rock!');
+      message = 'OK... Cat Slap beats Cat Punch!';
       winner = 'player';
       break;
     case (playerSelection === 'Paper' && computerSelection === 'Scissors'):
-      console.log('Unlucky ass! Scissors beat Paper!');
+      message = 'As expected of me! Cat Scratch beats Cat Slap!';
       winner = 'computer';
       break;
     case (playerSelection === 'Scissors' && computerSelection === 'Paper'):
-      console.log('Nice one! Scissors beat Paper');
+      message = 'Suspicious... but Cat Scratch beats Cat Slap';
       winner = 'player';
       break;
     case (playerSelection === 'Scissors' && computerSelection === 'Rock'):
-      console.log('You\'re quite bad! Rock beats Scissors');
+      message = 'Naturally I won! Cat Punch beats Cat Scratch';
       winner = 'computer';
       break;
-    default: 
-      console.log('Do you really know how to play this?');
-      process.exit();
-      //break;
   }
+  showMessage(message);
   return winner;
 }
+// this part is made to change to another screen when the game start
+document.querySelector('#trigger-game').addEventListener('click', () => {
+  document.querySelector('#container1').style.display = 'none';
+  document.querySelector('#container2').style.display = 'flex';
+  playGame();
+});
 
-function Game(){
-  /*This function simulates 5 rounds of rock-paper-scissors, the first one to get 3 points
-    or the one with more points at the end wins*/
-  
-  console.log('You will be playing 5 times, the one who first gets 3/5 wins\n Good luck!');
-  let playerSelection, winner;
-  let player_count = 0;
-  let computer_count = 0;
+function playGame(){
+  // This function simulates the game
+  let winner;
+  let playerScore = 0;
+  let computerScore = 0;
 
-  for (let i=0; i<5; i++){
-    playerSelection = window.prompt('Pick one: Rock, Paper, Scissors');  // imput from window
-    winner = playRound(playerSelection, computerPlay());
-    
-    if (winner === 'computer') {computer_count++;
-    } else if (winner === 'player') {player_count++;}
-    
-    if (computer_count === 3) {
-      console.log('3/5! Computer wins! Better luck next time!');
-      return;
-    }
-    if (player_count === 3) {
-      console.log('3/5! You win! What a boss!');
-      return;
-    }
-    if (player_count === 2 && computer_count === 2) console.log('Draw! Last one standing wins!');
-  }
-  console.log('player score: ' + player_count);
-  console.log('computer score: ' + computer_count);
-  
-  if (player_count > computer_count) {
-    console.log('You win! What a boss!');
-  } else if (computer_count > player_count) {
-    console.log('Computer wins! Better luck next time!');
-  } else {
-    console.log('A draw! We both suck!');
-  }
-  return;
+  const scoreHooman = document.querySelector('.hooman-score');
+  const scoreCat = document.querySelector('.cat-score');
+
+  // add an eventListener to each button the player can click in the game
+  Array.from(document.querySelectorAll('button')).forEach( 
+    element => element.addEventListener('click', () => {
+      winner = playRound(element.id, computerPlay())  // play a round after the player choose
+      if (winner === 'player') {
+        playerScore++;
+        scoreHooman.textContent = playerScore;  // update the scoreboard
+      } else if (winner === 'computer') {
+        computerScore++;
+        scoreCat.textContent = computerScore;
+      }
+
+      // condition to check for a winner
+      if (playerScore === 3 || computerScore === 3) {
+        playerScore > computerScore ? winner = 'Hoomans' : winner = 'Cats';  // check who won
+        // change scrren again after the game end, insert a message and an image for the winner
+        document.querySelector('#container2').style.display = 'none';
+        const container3 = document.querySelector('#container3');
+        container3.style.display = 'flex';
+        document.querySelector('.winner').textContent += winner + '!!';
+        const winnerImg = document.createElement('img');
+        if (winner === 'Hoomans') {
+          winnerImg.setAttribute('src', 'img/hooman-wins.jpg');
+          winnerImg.setAttribute('alt', 'img if hooman wins');
+        } else {
+          winnerImg.setAttribute('src', 'img/cat-wins.jpg');
+          winnerImg.setAttribute('alt', 'img if cat wins');
+        }
+        container3.appendChild(winnerImg);
+      }
+    })
+  );
 }
